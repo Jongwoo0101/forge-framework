1a44acc118dbd3b7483c6fe5037813021ab73b83
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-08-05 12:36:23 +0900

    chore: Main에 있는 TODO 삭제

--- src/main/java/forgeframework/Main.java
+++ src/main/java/forgeframework/Main.java
@@ -41,5 +41,4 @@ public final class Main {
         System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
         System.setErr(new PrintStream(System.err, true, StandardCharsets.UTF_8));
     }
-    // TODO: Frame Table, Virtual Memory 구현 후 Phase4 넘어가기
 }
